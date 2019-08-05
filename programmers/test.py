532a00725da6845ef8bc935af1a8403696665a76
Kim Young-in <chris9390@naver.com>
2019-08-05 20:13:41 +0900

    시간 복잡도 때문에 고생한 문제.
    "if n not in visited:" 와 같은 문법이 시간을 많이 잡아 먹는다.

--- programmers/test.py
+++ programmers/test.py
@@ -1,48 +1,67 @@
-import copy
-
-def dfs(graph, start):
-    visited = []
-    stack = [start]
-
-    while(stack):
-
-        # 모든 곳을 한번씩 다 거치면 이동 경로 반환
-        if set(graph.keys()) == set(visited):
-            return visited
-
-        n = stack.pop()
-
-        if n not in visited:
-            visited.append(n)
-            stack.extend(graph[n])
-        else:
-            visited.append(n)
-
-    return visited
+# 가장 먼 노드
+# https://programmers.co.kr/learn/courses/30/lessons/49189
 
 
-'''
-def dfs_recur(graph, start, visited):
-    
-    new_visited = copy.deepcopy(visited)
-    new_visited.append(start)
-    print(start)
+def bfs(graph, start):
+    queue = [start]
+    # 1번 인덱스가 1번 노드를 가리키기 위해서 distance 배열의 개수를 하나 더 늘려준다.
+    distance = [0] * (len(graph) + 1)
+    distance[0] = 0
+    distance[1] = 0
+
+    visited = [False] * (len(graph) + 1)
+    visited[0] = True
+    visited[1] = True
+
+    while(queue):
+        n = queue.pop(0)
+
+        for i in graph[n]:
+            # i 노드를 방문하지 않았으면 방문한다.
+            if visited[i] == False:
+                visited[i] = True
+                # i 노드의 주변 노드를 방문하기 위해 queue 에 추가.
+                queue.append(i)
+                # n 노드와 i 노드가 연결되어 있으므로 (n 노드까지의 최소 거리 + 1) 이 i 노드 까지의 최소 거리이다.
+                distance[i] = distance[n] + 1
 
 
-    for i in graph[start]:
-        if i not in new_visited:
-            dfs_recur(graph, i, new_visited)
-'''
+    return distance
 
 
 
-graph = {1 : [2],
-         2 : [3,4],
-         3 : [1,5],
-         4 : [2],
-         5 : [3,4]}
-start = 1
-visited = dfs(graph, start)
-#visited = []
-#dfs_recur(graph, start, visited)
-print(visited)
+def solution(n, edge):
+
+    graph = {}
+
+    # 그래프 키 초기화
+    for i in range(1, n+1):
+        graph[i] = []
+
+
+    # 그래프 제작
+    for i in edge:
+        # 중복 방지를 위한 if 문
+        if i[1] not in graph[i[0]]:
+            graph[i[0]].append(i[1])
+
+        # 중복 방지를 위한 if 문
+        if i[0] not in graph[i[1]]:
+            graph[i[1]].append(i[0])
+
+
+    distance = bfs(graph, 1)
+    maximum = max(distance)
+    return distance.count(maximum)
+
+
+
+
+n = 6
+edge = [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]
+#edge = [[1,2]]
+#edge = [[1,2], [1,3], [2,3],[2,5],[2,4],[3,6],[3,8],[3,5],[4,7],[6,8],[5,9],[2,9],[3,7],[1,7]]
+#edge = [[1,2],[2,3],[3,5],[1,4],[4,5]]
+#edge = [[1,2],[2,3],[3,4]]
+answer = solution(n, edge)
+print(answer)
